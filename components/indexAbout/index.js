eeda1b21e5a2e316d4e04730c941199fd210329e
WilliamThomsonNZ <william__thomson@outlook.com>
2022-05-30 16:52:35 +1200

    fix build issue

--- components/indexAbout/index.js
+++ components/indexAbout/index.js
@@ -47,10 +47,10 @@ const IndexAbout = () => {
             framework for both you and I. <br />
             <br /> There are many variations of passages of Lorem Ipsum
             available, but the majority have suffered alteration in some form,
-            by injected humour, or randomised words which don't look even
+            by injected humour, or randomised words which don&apos;t look even
             slightly believable. If you are going to use a passage of Lorem
-            Ipsum, you need to be sure there isn't anything embarrassing hidden
-            in the middle of text. All the Lorem Ipsum generators on the
+            Ipsum, you need to be sure there isn&apos;t anything embarrassing
+            hidden in the middle of text. All the Lorem Ipsum generators on the
             Internet tend to repeat predefined chunks as necessary, making this
             the first true generator on the Internet
           </motion.p>
