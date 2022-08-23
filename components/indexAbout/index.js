401bf74ad5e4b6797d1322bc13840cf132f9ec02
WilliamThomsonNZ <william__thomson@outlook.com>
2022-08-23 16:25:23 +1200

    Fix issues

--- components/indexAbout/index.js
+++ components/indexAbout/index.js
@@ -65,14 +65,30 @@ const IndexAbout = () => {
       >
         <Image src={"/will2.JPG"} height={1000} width={1000} />
       </motion.div>
-      <p className={styles.mobileDescription}>
+      <motion.p
+        variants={ProjectVariants.infoText}
+        whileInView={"whileInView"}
+        viewport={{ once: true }}
+        className={styles.mobileDescription}
+      >
         If you have got to this point , you are probably ready to find out more
         about me. So Hey! I’m Will, a passionate web3 developer and strategist
         currently based in Auckland, New Zealand. I help projects achieve
         success through performant decentralised application development, strong
         understanding of user motivation and the ability to advise on UI/UX
-        design decisions to ensure user’s are engaged and satisfied.
-      </p>
+        design decisions to ensure user’s are engaged and satisfied. <br /> 
+        <br /> Non fungible tokens ( NFTs ) are here to stay and will play a
+        major part in our online identities. My goal is to integrate this
+        technology into communities and businesses to improve the users overall
+        experience. By focusing directly on the user, I am able to understand
+        their motivation and create an experience and product that is bespoke
+        and engaging.
+        <br />
+        <br />
+        If you would like to chat about your project/vision/idea or just want to
+        have a conversation around how this technology can improve your
+        business, Shoot me a message.
+      </motion.p>
     </section>
   );
 };
